689ea6aac83b5cb65278203dfe709030c120f60d
Luisby2004 <luistoto745@gmail.com>
2023-04-24 22:41:25 -0500

    first commit

---
+++ src/main/java/com/cibertec/DawiClase06Application.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiClase06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiClase06Application.class, args);
+	}
+
+}
